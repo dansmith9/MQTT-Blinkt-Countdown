b07198d821dbd889fa8529510f335f3a86ff9147
Dan Smith <dansmith9@outlook.com>
2016-12-27 22:25:16 +0000

    As previous

--- MQTTcountdowntimer.py
+++ MQTTcountdowntimer.py
@@ -1,4 +1,4 @@
-from time import localtime, sleep
+from time import localtime, sleep, time
 import paho.mqtt.client as mqtt
 from blinkt import set_pixel, show, clear
 
@@ -57,111 +57,127 @@ client.loop_start()
 
 #**** Main loop ****
 
+print(time())
+print(time()+10)
+
+sleep(10)
+
+
+print(time())
+
 while True:
     if t>0:
-        print(t)
-        if status=="active":
-            print("Countdown",t)
-            mins,secs = divmod(t,60)
-            print("t =",t)
-            print(mins,secs)
-            lights=[]
-            if t>=60:
-                if mins>=128:
-                    lights.append("green")
-                    mins=mins-128
-                else:
-                    lights.append(0)
-                if mins>=64:
-                    lights.append("green")
-                    mins=mins-64
-                else:
-                    lights.append(0)
-                if mins>=32:
-                    lights.append("green")
-                    mins=mins-32
-                else:
-                    lights.append(0)
-                if mins>=16:
-                    lights.append("green")
-                    mins=mins-16
-                else:
-                    lights.append(0)
-                if mins>=8:
-                    lights.append("green")
-                    mins=mins-8
-                else:
-                    lights.append(0)
-                if mins>=4:
-                    lights.append("green")
-                    mins=mins-4
-                else:
-                    lights.append(0)
-                if mins>=2:
-                    lights.append("green")
-                    mins=mins-2
-                else:
-                    lights.append(0)
-                if mins>=1:
-                    lights.append("green")
-                    mins=mins-1
-                else:
-                    lights.append(0)
-                print(lights)
-            elif t>8:
-                if secs>=128:
-                    lights.append("amber")
-                    secs=secs-128
-                else:
-                    lights.append(0)
-                if secs>=64:
-                    lights.append("amber")
-                    secs=secs-64
-                else:
-                    lights.append(0)
-                if secs>=32:
-                    lights.append("amber")
-                    secs=secs-32
-                else:
-                    lights.append(0)
-                if secs>=16:
-                    lights.append("amber")
-                    secs=secs-16
-                else:
-                    lights.append(0)
-                if secs>=8:
-                    lights.append("amber")
-                    secs=secs-8
-                else:
-                    lights.append(0)
-                if secs>=4:
-                    lights.append("amber")
-                    secs=secs-4
-                else:
-                    lights.append(0)
-                if secs>=2:
-                    lights.append("amber")
-                    secs=secs-2
-                else:
-                    lights.append(0)
-                if secs>=1:
-                    lights.append("amber")
-                    secs=secs-1
-                else:
-                    lights.append(0)
-                print(lights)
-            elif t>=1:
-                for i in range(8,0,-1):
-                    if i>t:
-                        lights.append(0)
+        time_remaining=t-time()
+        print(time_remaining)
+        if time_remaining>=0:
+            print(time_remaining)
+            print(t)
+            if status=="active":
+                print("Countdown",time_remaining)
+                mins,secs = divmod(time_remaining,60)
+                print("t =",time_remaining)
+                print(mins,secs)
+                lights=[]
+                if time_remaining>=60:
+                    if mins>=128:
+                        lights.append("green")
+                        mins=mins-128
                     else:
-                        lights.append("red")
-                print(lights)
-            t-=1
-            sleep(1)
-        elif status=="abort":
-            print("Stopping...")
-            clear()
-            show()
+                        lights.append(0)
+                    if mins>=64:
+                        lights.append("green")
+                        mins=mins-64
+                    else:
+                        lights.append(0)
+                    if mins>=32:
+                        lights.append("green")
+                        mins=mins-32
+                    else:
+                        lights.append(0)
+                    if mins>=16:
+                        lights.append("green")
+                        mins=mins-16
+                    else:
+                        lights.append(0)
+                    if mins>=8:
+                        lights.append("green")
+                        mins=mins-8
+                    else:
+                        lights.append(0)
+                    if mins>=4:
+                        lights.append("green")
+                        mins=mins-4
+                    else:
+                        lights.append(0)
+                    if mins>=2:
+                        lights.append("green")
+                        mins=mins-2
+                    else:
+                        lights.append(0)
+                    if mins>=1:
+                        lights.append("green")
+                        mins=mins-1
+                    else:
+                        lights.append(0)
+                    print(lights)
+                elif time_remaining>8:
+                    if secs>=128:
+                        lights.append("amber")
+                        secs=secs-128
+                    else:
+                        lights.append(0)
+                    if secs>=64:
+                        lights.append("amber")
+                        secs=secs-64
+                    else:
+                        lights.append(0)
+                    if secs>=32:
+                        lights.append("amber")
+                        secs=secs-32
+                    else:
+                        lights.append(0)
+                    if secs>=16:
+                        lights.append("amber")
+                        secs=secs-16
+                    else:
+                        lights.append(0)
+                    if secs>=8:
+                        lights.append("amber")
+                        secs=secs-8
+                    else:
+                        lights.append(0)
+                    if secs>=4:
+                        lights.append("amber")
+                        secs=secs-4
+                    else:
+                        lights.append(0)
+                    if secs>=2:
+                        lights.append("amber")
+                        secs=secs-2
+                    else:
+                        lights.append(0)
+                    if secs>=1:
+                        lights.append("amber")
+                        secs=secs-1
+                    else:
+                        lights.append(0)
+                    print(lights)
+                elif time_remaining>=1:
+                    for i in range(8,0,-1):
+                        if i>t:
+                            lights.append(0)
+                        else:
+                            lights.append("red")
+                    print(lights)
+                #t-=1
+                sleep(1)
+            elif status=="abort":
+                print("Stopping...")
+                clear()
+                show()
+                t=0
+                time_remaining=0
+        else:
             t=0
+            time_remaining=0
     
